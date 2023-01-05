0977451b0ffe32c05d3979acce012d5877a56078
Tyler Gunlock <ty_gunlock@yahoo.com>
2023-01-05 15:41:42 -0500

    Routine_activities.spec all tests passing

--- db/routine_activities.js
+++ db/routine_activities.js
@@ -25,15 +25,96 @@ async function addActivityToRoutine({
   
 }
 
-async function getRoutineActivityById(id) {}
+async function getRoutineActivityById(id) {
+  try {
+    const { rows: [routineActivity] } = await client.query(`
+      SELECT *
+      FROM routine_activities
+      WHERE id=$1
+    `, [id]);
 
-async function getRoutineActivitiesByRoutine({ id }) {}
+    return routineActivity;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
 
-async function updateRoutineActivity({ id, ...fields }) {}
+async function getRoutineActivitiesByRoutine({ id }) {
+  try {
+    const { rows: routineActivities } = await client.query(`
+      SELECT *
+      FROM routine_activities
+      WHERE "routineId"=$1;
+    `, [id]);
 
-async function destroyRoutineActivity(id) {}
+    return routineActivities;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
 
-async function canEditRoutineActivity(routineActivityId, userId) {}
+async function updateRoutineActivity({ id, ...fields }) {
+  const keys = Object.keys(fields);
+
+  const setString = keys.map(
+    (key, index) => `"${key}"=$${index + 1}`
+  ).join(', ');
+  
+  try { 
+    if (setString.length > 0) {
+      const {rows: [updatedRoutineActivity]} = await client.query (`
+        UPDATE routine_activities
+        SET ${setString}
+        WHERE id=${id}
+        RETURNING *;
+      `, Object.values(fields));
+
+      return updatedRoutineActivity;
+    } else {
+      return null;
+    }
+
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+async function destroyRoutineActivity(id) {
+  try {
+    const {rows: [deletedRoutineActivity]} = await client.query(`
+      DELETE FROM routine_activities
+      WHERE id = $1
+      RETURNING *;
+    `, [id]);
+
+    return deletedRoutineActivity;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+async function canEditRoutineActivity(routineActivityId, userId) {
+  try {
+    const { rows: [editRoutineActivity] } = await client.query(`
+      SELECT routine_activities."routineId", routines."creatorId"
+      FROM routine_activities
+      JOIN routines ON routine_activities."routineId" = routines.id
+      WHERE routine_activities.id=$1
+    `, [routineActivityId]);
+    if (editRoutineActivity.creatorId === userId) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
 
 module.exports = {
   getRoutineActivityById,
